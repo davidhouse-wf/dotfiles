fb7b5978ed154fdfb7646ffde482062fd3cb2954
Dave House <dave.a.house@gmail.com>
2016-09-09 09:24:01 -0700

    allow image&video preview in ranger

--- brew.sh
+++ brew.sh
@@ -102,5 +102,8 @@ brew install vbindiff
 #brew install webkit2png
 #brew install zopfli
 
+brew install w3m
+brew install ffmpegthumbnailer
+
 # Remove outdated versions from the cellar.
 brew cleanup
